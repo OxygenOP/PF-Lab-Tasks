c57b3dce58c4f957e626874a7a3b05d256440593
Hassam Sohail <hassamsohail86@gmail.com>
2024-05-11 10:15:10 +0500

    Update main.cpp

--- Task6/Calculator/main.cpp
+++ Task6/Calculator/main.cpp
@@ -11,7 +11,6 @@ int main(){
 
     cout << "This is a Calculator \nMade by Hassam Sohail \n\n\n";
 
-
     
     
 
@@ -27,11 +26,11 @@ while(true){
     // Get User Input
     cout << "Calculator Menu" << endl;
     cout << "(0) to exit calculator" << endl;
-    cout << "(+ or 1) to perform addition" << endl;
-    cout << "(- or 2) to perform substraction" << endl;
-    cout << "(* or 3) to perform multiplication" << endl;
-    cout << "(/ or 4) to perform division" << endl;
-    cout << "(\% or 5) to perform modulus" << endl;
+    cout << "(+) to perform addition" << endl;
+    cout << "(-) to perform substraction" << endl;
+    cout << "(*) to perform multiplication" << endl;
+    cout << "(/) to perform division" << endl;
+    cout << "(\%) to perform modulus" << endl;
     cout << ">> ";
 
 
@@ -54,35 +53,40 @@ while(true){
     //Perform Arithmetic Operation
 
     
-    if(operation == '1' || operation == '+'){
+
+
+ switch ( operation)
+    {
+    case '+':
         result = num1+num2;
-    }
-    else if(operation == '2' || operation == '-'){
-                result = num1-num2; 
-    }
-    else if(operation == '3' || operation == '*'){
-                result = num1*num2; 
-    }
-    else if(operation == '4' || operation == '/'){
-
-        if(num2 == 0){
-
-            wrongInput = true;
-            cout << "Number cannot be divided by zero" << endl;
-
-        }
+        break;
+     case '-':
+        result = num1-num2;
+        break;
+     case '*':
+        result = num1*num2;
+        break;
+     case '/':
+     if(num2 == 0){
+        wrongInput = true;
+        cout << "Number cannot be divided by zero" << endl;
+        break;
+     }
         result = num1/num2;
+        break;
+     case '%':
+        result = fmod(num1, num2);
+        break;
+    default:
+        wrongInput = true;
+        break;
     }
-    else if(operation == '5' || operation == '%'){
-                result = fmod(num1, num2);
-    }
-    else {
-                wrongInput = true;
-    }
+
+
+
 
 
     //Output to user
-
     if(wrongInput){
         cout << "The Form of Input was incorrect, or the arithmetic operation doesn't exist.." << endl;
     }
