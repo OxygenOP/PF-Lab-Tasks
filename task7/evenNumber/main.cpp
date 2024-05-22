9f9cd903172c7ce52b2349d4c377e38ea7a5938c
Hassam Sohail <hassamsohail86@gmail.com>
2024-05-22 22:50:18 +0500

    Add files via upload

---
+++ task7/evenNumber/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // Even number
+
+    int startRange, endRange;
+
+    cout << "Input Starting Number: ";
+    cin >> startRange;
+
+    cout << "Input Ending Number: ";
+    cin >> endRange;
+
+    for(int i = startRange; i <= endRange; i++ ){
+        if(i % 2 == 0){
+            cout << i << endl;
+        }
+
+    }
+
+    return 0;
+}
+
